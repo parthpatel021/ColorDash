f3020ca51a31a550b66a56fb24e3bc2a250ab3ea
Parth Patel <parthkumar111patel@gmail.com>
2023-12-21 11:01:43 +0530

    Score Functionality Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,63 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css'; // Import your CSS file
+import './App.css'; 
 
-const API_URL = 'http://localhost:8080'; // Update with your server endpoint
+const GridCell = (props) => {
+    const { cell, isRed } = props;
+    const [blinking, setBlinking] = useState(false);
+
+    const handleClick = () => {
+        if (isRed === true) {
+            props.changeScore(-10);
+        }
+        else if (cell === 1) {
+            props.changeScore(10);
+        }
+        setBlinking(true);
+        setTimeout(() => {
+            setBlinking(false);
+        }, "200")
+    }
+
+    return (
+        <div
+            className={`grid-cell ${blinking ? 'blink' : ' '}`}
+            style={{ backgroundColor: `${isRed ? 'red' : (cell === 1 ? 'blue' : 'black')}` }}
+            onMouseDown={handleClick}
+        />
+    );
+}
 
 const App = () => {
+    const [redBlockIdx, setRedBlockIdx] = useState(0);
     const [gameData, setGameData] = useState({
         grid: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ],
         score: 0,
         isGameRunning: false,
         animationSpeed: 250,
     });
 
-    const getGridPatten = async (req,res) => {
+    const getGridPatten = async (req, res) => {
         try {
-            const {data} = await axios.get(`${API_URL}/initialGrid`);
-            const gridPatten= data.grid;
-            console.log(gridPatten);
-            setGameData({grid: gridPatten, 
+            const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/initialGrid`);
+            const gridPatten = data.grid;
+            
+            setGameData({
+                grid: gridPatten,
                 score: 0,
                 isGameRunning: false,
-                animationSpeed: 250,});
+                animationSpeed: 250,
+            });
         } catch (error) {
             console.log(error);
         }
@@ -39,36 +65,49 @@ const App = () => {
 
     useEffect(() => {
         // Fetch initial data
-        getGridPatten();
+        getGridPatten();   
     }, []);
 
+    const handleStartGame = () => {
+        setGameData({ ...gameData, isGameRunning: true });
+    }
+
+    const handleStopGame = () => {
+        setGameData({ ...gameData, isGameRunning: false });
+    }
+
+    const changeScore = (dScore) => {
+        setGameData({ ...gameData, score: gameData.score + dScore });
+    }
 
 
     return (
         <div className="app-container">
             <h1>ColorDash Challenge: Tile Tapper</h1>
-            
+
             <div className="game-container">
                 <div className="controls-container">
                     <div className="score-container">Score: {gameData.score}</div>
-                    <button disabled={gameData.isGameRunning}>Start Game</button>
-                    <button disabled={!gameData.isGameRunning}>Stop Game</button>
+                    <button onClick={handleStartGame} disabled={gameData.isGameRunning}>Start Game</button>
+                    <button onClick={handleStopGame} disabled={!gameData.isGameRunning}>Stop Game</button>
                     <select disabled={gameData.isGameRunning}>
-                        <option value="1">Speed 1</option>
-                        <option value="2">Speed 2</option>
-                        <option value="3">Speed 3</option>
-                        <option value="4">Speed 4</option>
-                        <option value="5">Speed 5</option>
+                        <option key={1} value="1">Speed 1</option>
+                        <option key={2} value="2">Speed 2</option>
+                        <option key={3} value="3">Speed 3</option>
+                        <option key={4} value="4">Speed 4</option>
+                        <option key={5} value="5">Speed 5</option>
                     </select>
                 </div>
                 <div className="grid-container">
-                    {/* Render your 10x10 grid here and handle tile clicks */}
+                    {/* 10x10 grid  */}
                     {gameData.grid.map((row, rowIndex) => (
                         <>
                             {row.map((cell, colIndex) => (
-                                <div
-                                    key={colIndex}
-                                    className={`grid-cell ${cell === 1 ? 'blue' : 'red'}`}
+                                <GridCell
+                                    key={rowIndex*10+colIndex}
+                                    cell={cell}
+                                    isRed={(colIndex === redBlockIdx * 2 || colIndex === redBlockIdx * 2 + 1) ? true : false}
+                                    changeScore={changeScore}
                                 />
                             ))}
                         </>
